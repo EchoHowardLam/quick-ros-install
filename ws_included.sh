0a141020c9412dcec5c962a5b4747f91b5069329
EchoHowardLam <EchoHowardLam@users.noreply.github.com>
2021-06-10 14:35:07 +0800

    Update ws_included.sh

--- ws_included.sh
+++ ws_included.sh
@@ -2,8 +2,8 @@
 
 wget https://raw.githubusercontent.com/EchoHowardLam/quick-ros-install/master/ros_install.sh && \
 bash ros_install.sh $1 && \
-cd ~ && mkdir -p catkin_ws/src && \
+mkdir -p ~/catkin_ws/src && \
 echo "source /opt/ros/$1/setup.bash" >> ~/.bashrc && \
-echo "source ~/devel/setup.bash" >> ~/.bashrc && \
-source ~/.bashrc ;
-cd catkin_ws && catkin_make
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+cd ~/catkin_ws && catkin_make
