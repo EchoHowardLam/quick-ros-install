10fea154a06c60cff92bd2c2b5f082a652877b0c
EchoHowardLam <EchoHowardLam@users.noreply.github.com>
2021-12-02 17:22:14 +0800

    Fix the bug where bashrc is not sourced

--- ws_included.sh
+++ ws_included.sh
@@ -5,5 +5,4 @@ bash ros_install.sh $1 && \
 mkdir -p ~/catkin_ws/src && \
 echo "source /opt/ros/$1/setup.bash" >> ~/.bashrc && \
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-cd ~/catkin_ws && catkin_make
+bash -i -c 'cd ~/catkin_ws && catkin_make'
